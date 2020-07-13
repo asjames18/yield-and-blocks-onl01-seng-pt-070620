eb3c49c979f68f74aca940b4229b23f5258670ff
Antonio James <asjames18@gmail.com>
2020-07-13 20:14:45 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,15 +1,15 @@
-# `def hello_t(array)
-# name = 0
-#   while name < array.length
-#     yield(array[name])
-#   name += 1
-#     end
-# array
-#
-# end
-#
-# hello_t(" ") do |name|
-#   if name.start_with?("T")
-#     puts "Hi, #{name}"
-#   end
-# end`
+`def hello_t(array)
+name = 0
+  while name < array.length
+    yield(array[name])
+  name += 1
+    end
+array
+
+end
+
+hello_t(" ") do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end`
